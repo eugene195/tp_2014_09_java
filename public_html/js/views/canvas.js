48dd45b4f3cba08ad51f21261fe58f9655ec29d4
Eugene <eugene@EugeneSatellite.(none)>
2014-10-27 20:24:16 +0300

    Buttons added, canvas clear fixed

--- public_html/js/views/canvas.js
+++ public_html/js/views/canvas.js
@@ -6,85 +6,113 @@ define([
     tmpl
 ){
 var View = Backbone.View.extend({
-    el: $('.canvas'),
-    template: tmpl,
-    initialize: function () {
-        this.render();
-        this.$el.hide();
-    },
-    render: function () {
-        this.$el.html(this.template());
-    },
-    show: function () {
-        this.trigger('reshow', this);
+        el: $('.canvas'),
+        template: tmpl,
 
-        window.requestAnimFrame = (function(callback) {
-            return window.requestAnimationFrame || window.webkitRequestAnimationFrame || 
-                window.mozRequestAnimationFrame || window.oRequestAnimationFrame || 
-                window.msRequestAnimationFrame ||
-            function(callback) {
-              window.setTimeout(callback, 1000 / 60);
+        initialize: function () {
+            this.render();
+            this.$el.hide();
+        },
+        render: function () {
+            this.$el.html(this.template());
+        },
+
+        amplUpClick: function(event) {
+            this.amp += 100;
+        },
+
+        show: function () {
+            this.trigger('reshow', this);
+
+            window.requestAnimFrame = (function(callback) {
+                return window.requestAnimationFrame || window.webkitRequestAnimationFrame ||
+                    window.mozRequestAnimationFrame || window.oRequestAnimationFrame ||
+                    window.msRequestAnimationFrame ||
+                function(callback) {
+                  window.setTimeout(callback, 1000 / 60);
+                };
+            })();
+
+            var amp = 100;
+            var freq = 10;
+            var base = 200;
+
+            var Circle = {
+                x: 0,
+                y: 75,
+                radius: 20,
+                borderWidth: 2
             };
-        })();
-      
-        var amp = 100;
-        var freq = 10;
-        var base = 200;
 
-        var Circle = {
-            x: 0,
-            y: 75,
-            radius: 20,
-            borderWidth: 2
-        };
-      
-        function sinePoint(base, time, amp, freq) {
-            return base + (amp * Math.sin(freq * time * Math.PI));
-        }
-
-        function drawCircle(Circle, context) {
-            context.beginPath();
-            context.arc(Circle.x, Circle.y, Circle.radius, 0, 2 * Math.PI, false);
-            context.fillStyle = '#00FFFF';
-            context.fill();
-            context.lineWidth = Circle.borderWidth;
-            context.strokeStyle = '#003300';
-            context.stroke();
-        }
-
-        function animate(Circle, canvas, context, startTime) {
-            // update
-            var time = (new Date()).getTime() - startTime;
-
-            var linearSpeed = 100;
-            // pixels / second
-            var newX = linearSpeed * time / 1000;
-
-            if(newX < canvas.width - Circle.radius * 2) {
-                Circle.x = newX;
-                Circle.y = sinePoint(base, time/10000, amp, freq);
+            function sinePoint(base, time, amp, freq) {
+                return base + (amp * Math.sin(freq * time * Math.PI));
             }
 
-            // clear
-            context.clearRect(0, 0, canvas.width, canvas.height);
+            function drawCircle(Circle, context) {
+                context.beginPath();
+                context.arc(Circle.x, Circle.y, Circle.radius, 0, 2 * Math.PI, false);
+                context.fillStyle = '#00FFFF';
+                context.fill();
+                context.lineWidth = Circle.borderWidth;
+                context.strokeStyle = '#003300';
+                context.stroke();
+            }
 
-            drawCircle(Circle, context);
+            function animate(Circle, canvas, context, startTime) {
+                // update
+                var time = (new Date()).getTime() - startTime;
 
-            // request new frame
-            requestAnimFrame(function() {
-                animate(Circle, canvas, context, startTime);
+                var linearSpeed = 100;
+                // pixels / second
+                var newX = linearSpeed * time / 1000;
+
+                context.clearRect(Circle.x - Circle.radius * 2, Circle.y - Circle.radius * 2,
+                                  Circle.x + Circle.radius * 2, Circle.y + Circle.radius * 2);
+
+                if(newX < canvas.width - Circle.radius * 2) {
+                    Circle.x = newX;
+                    Circle.y = sinePoint(base, time/10000, amp, freq);
+                }
+
+                // clear
+//                context.clearRect(0, 0, canvas.width, canvas.height);
+
+
+                drawCircle(Circle, context);
+
+                // request new frame
+                requestAnimFrame(function() {
+                    animate(Circle, canvas, context, startTime);
+                });
+            }
+
+//          Event Listeners
+            document.getElementById('amplitudeUp').addEventListener('click', function() {
+                amp = amp + 100;
             });
+
+            document.getElementById('amplitudeDown').addEventListener('click', function() {
+                amp = amp - 100;
+            });
+
+            document.getElementById('frequencyUp').addEventListener('click', function() {
+                freq = freq - 100;
+            });
+
+            document.getElementById('frequencyDown').addEventListener('click', function() {
+                freq = freq + 100;
+            });
+
+            var canvas = document.getElementById('myCanvas');
+            var context = canvas.getContext('2d');
+
+            // wait one second before starting animation
+            setTimeout(function() {
+                var startTime = (new Date()).getTime();
+                animate(Circle, canvas, context, startTime);
+                }, 1000);
         }
 
-        var canvas = document.getElementById('myCanvas');
-        var context = canvas.getContext('2d');
-
-        // wait one second before starting animation
-        setTimeout(function() {
-            var startTime = (new Date()).getTime();
-            animate(Circle, canvas, context, startTime);
-            }, 1000);
-        }
     });
 
     return new View();
