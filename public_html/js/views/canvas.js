dae65dfd5b72a8d0f08bd0aeeb55f0a547393ef3
xammi-1 <kmg.xammi.1@gmail.com>
2014-10-28 18:07:25 +0400

    A little bit more refactor

--- public_html/js/views/canvas.js
+++ public_html/js/views/canvas.js
@@ -7,6 +7,50 @@ define([
     tmpl,
     Manager
 ){
+
+    function Point (x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    function Button(start, length, text, height, btnName) {
+        this.start = start;
+        this.length = length;
+        this.text = text;
+        this.height = height;
+        this.btnName = btnName;
+
+        this.drawButton = function(context) {
+            context.beginPath();
+            context.rect(this.start, 0, this.length, this.height);
+            context.fillStyle = 'yellow';
+            context.fill();
+            context.font = '12pt Calibri';
+            context.lineWidth = 1;
+            context.strokeStyle = 'blue';
+            context.strokeText(this.text, this.start + 5, this.height / 2);
+            context.lineWidth = 2;
+            context.strokeStyle = 'black';
+            context.stroke();
+        };
+
+        this.isInBounds = function(x, y) {
+            if ((x >  this.start) && (x < this.start + this.length) && (y > 0) && (y < this.height)) {
+                return true;
+            }
+            return false;
+        };
+    }
+
+    function sinePoint(base, time, amp, freq) {
+        return base + (amp * Math.sin(freq * time * Math.PI));
+    }
+
+    function getMousePos(canvas, evt) {
+        var rect = canvas.getBoundingClientRect();
+        return new Point(evt.clientX - rect.left, evt.clientY - rect.top);
+    }
+
     var View = Backbone.View.extend({
         el: $('.canvas'),
         template: tmpl,
@@ -43,6 +87,37 @@ define([
                 };
             })();
 
+            var amp = 100;
+            var freq = 40;
+            var base = 200;
+
+            var self = this;
+
+            var canvas = document.getElementById('myCanvas');
+            var context = canvas.getContext('2d');
+
+            var btnCanvas = document.getElementById('btnCanvas');
+            var btnContext = btnCanvas.getContext('2d');
+
+            var btnArray = [
+                new Button(0, 150, "Frequency Up", 50, "freqUp"),
+                new Button(160, 150, "Frequency Down", 50, "freqDown"),
+                new Button(320, 150, "Amplitude Up", 50, "ampUp"),
+                new Button(480, 150, "Amplitude Down", 50, "ampDown")
+            ];
+
+            btnArray.forEach(function(entry) {
+                entry.drawButton(btnContext);
+            });
+
+            btnCanvas.addEventListener('click', function(evt) {
+                var mousePos = getMousePos(btnCanvas, evt);
+                btnArray.forEach(function(entry) {
+                    if (entry.isInBounds(mousePos.x, mousePos.y)) {
+                        buttonPress(entry.btnName);
+                    }
+                });
+            }, false);
 
             function buttonPress(btnName) {
                 if (btnName == "freqUp") {
@@ -55,19 +130,6 @@ define([
                     amp = amp - 10;
                 }
             };
-
-            var btnArray = [
-                new Button(0, 150, "Frequency Up", 50, "freqUp"),
-                new Button(160, 150, "Frequency Down", 50, "freqDown"),
-                new Button(320, 150, "Amplitude Up", 50, "ampUp"),
-                new Button(480, 150, "Amplitude Down", 50, "ampDown")
-            ];
-
-            var amp = 100;
-            var freq = 40;
-            var base = 200;
-
-            var self = this;
 
             function Circle(x, y, radius, borderWidth) {
                 this.x = x;
@@ -121,14 +183,11 @@ define([
                 }
             }
 
-            function sinePoint(base, time, amp, freq) {
-                return base + (amp * Math.sin(freq * time * Math.PI));
-            }
-
-            function getMousePos(canvas, evt) {
-                var rect = canvas.getBoundingClientRect();
-                return new Point(evt.clientX - rect.left, evt.clientY - rect.top);
-            }
+            setTimeout(function() {
+                var startTime = (new Date()).getTime();
+                var circle = new Circle(0, 75, 20, 2);
+                circle.animate(canvas, context, startTime);
+            }, 1000);
 
             var tail = [];
 
@@ -145,75 +204,9 @@ define([
                 }
                 context.stroke();
             }
-
-
-            var canvas = document.getElementById('myCanvas');
-            var context = canvas.getContext('2d');
-
-            var btnCanvas = document.getElementById('btnCanvas');
-            var btnContext = btnCanvas.getContext('2d');
-
-            btnArray.forEach(function(entry) {
-                entry.drawButton(btnContext);
-            });
-
-            btnCanvas.addEventListener('click', function(evt) {
-                var mousePos = getMousePos(btnCanvas, evt);
-                btnArray.forEach(function(entry) {
-                    if (entry.isInBounds(mousePos.x, mousePos.y)) {
-                        buttonPress(entry.btnName);
-                    }
-                });
-
-            }, false);
-//            What is that false?
-
-            setTimeout(function() {
-
-                var startTime = (new Date()).getTime();
-                var circle = new Circle(0, 75, 20, 2);
-                circle.animate(canvas, context, startTime);
-
-            }, 1000);
         }
 
     });
 
     return new View();
 });
-
-
-function Point (x, y) {
-    this.x = x;
-    this.y = y;
-}
-
-
-function Button(start, length, text, height, btnName) {
-    this.start = start;
-    this.length = length;
-    this.text = text;
-    this.height = height;
-    this.btnName = btnName;
-
-    this.drawButton = function(context) {
-        context.beginPath();
-        context.rect(this.start, 0, this.length, this.height);
-        context.fillStyle = 'yellow';
-        context.fill();
-        context.font = '12pt Calibri';
-        context.lineWidth = 1;
-        context.strokeStyle = 'blue';
-        context.strokeText(this.text, this.start + 5, this.height / 2);
-        context.lineWidth = 2;
-        context.strokeStyle = 'black';
-        context.stroke();
-    }
-
-    this.isInBounds = function(x, y) {
-        if ((x >  this.start) && (x < this.start + this.length) && (y > 0) && (y < this.height)) {
-            return true;
-        }
-        return false;
-    }
-};
